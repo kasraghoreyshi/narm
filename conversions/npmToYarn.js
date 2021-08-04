e3b166b46a4c7f9f877bddcd6a3bcd762b020cbf
Kasra Ghoreyshi <kasra.ghoreyshi84@gmail.com>
2021-08-04 16:20:18 +0430

    Removed unused files

--- conversions/npmToYarn.js
+++
@@ -1,4 +0,0 @@
-const { yarn } = require("../package-managers/npm");
-const { convert } = require("./helper");
-
-module.exports = convert(command, yarn);
--- conversions/yarnToNpm.js
+++
@@ -1,3 +0,0 @@
-const { npm } = require("../package-managers/npm");
-
-module.exports = convert(command, npm);
