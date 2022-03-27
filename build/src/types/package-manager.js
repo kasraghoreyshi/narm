844553ec02a35d56dfbad0f6fa3bce006f94dc58
kasraghoreyshi <kasra.ghoreyshi84@gmail.com>
2022-03-27 15:03:12 +0430

    Fix untracked files

--- build/src/types/package-manager.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.packageManagers = void 0;
-const npm_1 = require("../package-managers/npm");
-const yarn_1 = require("../package-managers/yarn");
-exports.packageManagers = {
-    npm: npm_1.npm,
-    yarn: yarn_1.yarn,
-};
